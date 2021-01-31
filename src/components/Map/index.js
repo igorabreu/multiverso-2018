f602c170286c04638bb303a9681a2760f035655d
Igor Abreu <igor.abreu.reis@gmail.com>
2021-01-31 17:50:20 -0300

    feat(Map): update API_KEY settings

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -4,6 +4,7 @@ import GoogleMapReact from 'google-map-react'
 import MapMarker from '../MapMarker'
 import mapProps from './mapProps.js'
 import locations from './locations'
+import { API_KEY } from '../../utils/credentials.js'
 import AnimatedGraphism from '../AnimatedGraphism'
 import './style.scss'
 import AnimatedTitle from '../AnimatedTitle'
@@ -11,43 +12,48 @@ import AnimatedTitle from '../AnimatedTitle'
 class Map extends Component {
   static defaultProps = {
     ...mapProps,
-  }
-
+  }z
   render() {
     const { zoom, center, styles } = this.props
-    return (
-      <div className="Map">
-        <div className="taglines">
-          <h3>Creative Coding</h3>
-          <h3>Arte Generativa</h3>
+    if (process.env.API_KEY_GOOGLE_MAPS) {
+      return (
+        <div className="Map">
+          <div className="taglines">
+            <h3>Creative Coding</h3>
+            <h3>Arte Generativa</h3>
+          </div>
+          <AnimatedGraphism />
+          <AnimatedTitle />
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: process.env.API_KEY_GOOGLE_MAPS }}
+            defaultCenter={center}
+            defaultZoom={zoom}
+            options={{
+              styles: [...styles],
+              disableDefaultUI: true,
+              minZoom: 12,
+            }}
+          >
+            {locations.map((location, index) => {
+              return (
+                <MapMarker
+                  key={index}
+                  title={location.title}
+                  lat={location.lat}
+                  lng={location.lng}
+                />
+              )
+            })}
+          </GoogleMapReact>
         </div>
-        <AnimatedGraphism />
-        <AnimatedTitle />
-        <GoogleMapReact
-          bootstrapURLKeys={{
-            key: process.env.API_KEY_GOOGLE_MAPS,
-          }}
-          defaultCenter={center}
-          defaultZoom={zoom}
-          options={{
-            styles: [...styles],
-            disableDefaultUI: true,
-            minZoom: 12,
-          }}
-        >
-          {locations.map((location, index) => {
-            return (
-              <MapMarker
-                key={index}
-                title={location.title}
-                lat={location.lat}
-                lng={location.lng}
-              />
-            )
-          })}
-        </GoogleMapReact>
-      </div>
-    )
+      )
+    } else {
+      return (
+        <div className="Map">
+          <h3 className="message-error"> Multiverso - Mapa de Atividades </h3>
+        </div>
+      )
+    }
   }
 }
 export default Map
