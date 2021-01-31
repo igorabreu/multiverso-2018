446ed2268881f68c3d3f30bae22e72d5c62aa147
Igor Abreu <igor.abreu.reis@gmail.com>
2021-01-31 17:45:37 -0300

    feat(Map): update API_KEY settings

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -13,58 +13,41 @@ class Map extends Component {
     ...mapProps,
   }
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      API_KEY: undefined,
-    }
-  }
-
-  componentDidMount() {
-    setState({ API_KEY: process.env.API_KEY_GOOGLE_MAPS })
-  }
-
   render() {
     const { zoom, center, styles } = this.props
-    if (this.state.API_KEY) {
-      return (
-        <div className="Map">
-          <div className="taglines">
-            <h3>Creative Coding</h3>
-            <h3>Arte Generativa</h3>
-          </div>
-          <AnimatedGraphism />
-          <AnimatedTitle />
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: this.state.API_KEY }}
-            defaultCenter={center}
-            defaultZoom={zoom}
-            options={{
-              styles: [...styles],
-              disableDefaultUI: true,
-              minZoom: 12,
-            }}
-          >
-            {locations.map((location, index) => {
-              return (
-                <MapMarker
-                  key={index}
-                  title={location.title}
-                  lat={location.lat}
-                  lng={location.lng}
-                />
-              )
-            })}
-          </GoogleMapReact>
+    return (
+      <div className="Map">
+        <div className="taglines">
+          <h3>Creative Coding</h3>
+          <h3>Arte Generativa</h3>
         </div>
-      )
-    } else {
-      return (
-        <div className="Map">
-          <h3 className="message-error"> Missing Google Maps API Key </h3>
-        </div>
-      )
-    }
+        <AnimatedGraphism />
+        <AnimatedTitle />
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: process.env.API_KEY_GOOGLE_MAPS,
+          }}
+          defaultCenter={center}
+          defaultZoom={zoom}
+          options={{
+            styles: [...styles],
+            disableDefaultUI: true,
+            minZoom: 12,
+          }}
+        >
+          {locations.map((location, index) => {
+            return (
+              <MapMarker
+                key={index}
+                title={location.title}
+                lat={location.lat}
+                lng={location.lng}
+              />
+            )
+          })}
+        </GoogleMapReact>
+      </div>
+    )
   }
 }
 export default Map
